267dc167762adfc0745d6eb0e2a9b8207b0a7742
katejsenger <katejsenger@yahoo.com>
2018-10-29 23:42:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,12 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-
+def batch_badge_creator(array)
+  a = []
+  array.each do |name|
+    a.push("Hello, my name is #{name}.")
+  end
+  a
+end
 
   
