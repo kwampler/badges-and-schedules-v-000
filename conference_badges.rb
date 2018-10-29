1d9f4689036aaee2bcaa7b5eb993b7d8e4180b94
katejsenger <katejsenger@yahoo.com>
2018-10-29 23:47:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,4 +17,12 @@ def assign_rooms(array)
   end
   a
 end
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
   
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
